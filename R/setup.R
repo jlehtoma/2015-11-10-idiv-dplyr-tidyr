1c00a2034ef2b4c071fb3fc2528024f8c7aef2be
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2015-05-03 12:21:54 +0300

    Update data downloads

--- R/setup.R
+++ R/setup.R
@@ -16,3 +16,13 @@ if (!file.exists("data/plots.csv")) {
   download.file("http://files.figshare.com/1919738/plots.csv",
                 "data/plots.csv")
 }
+
+if (!file.exists("data/messy_survey_data.csv")) {
+  download.file("http://sorvivalas.com/temp/messy_survey_data.csv",
+                "data/messy_survey_data.csv")
+}
+
+if (!file.exists("data/pivot_survey_data.csv")) {
+  download.file("http://sorvivalas.com/temp/pivot_survey_data.csv",
+                "data/pivot_survey_data.csv")
+}
